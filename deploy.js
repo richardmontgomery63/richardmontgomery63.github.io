943f3f6e2dce2a73170a9bca08025d3107122d57
richard <richardmontgomery1936@gmail.com>
2025-02-16 01:21:45 -0700

    yes lord

--- deploy.js
+++ deploy.js
@@ -1,6 +1,8 @@
 import ghpages from 'gh-pages';
 
 ghpages.publish('dist', (err) => {
-  console.error(err);
-  process.exit(1);
+  if (err) {
+    console.error(err);
+    process.exit(1);
+  }
 });
